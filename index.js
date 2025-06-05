7ca97f398ff0fccf458aab7b3ebbcd62bc542d9a
tomkndn <ntamsoy66@gmail.com>
2025-06-05 10:50:18 +0530

    some minor fixes done for submission

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 const express = require("express");
 const { v4: uuidv4 } = require("uuid");
+require("dotenv").config();
 
 const app = express();
 app.use(express.json());
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 
 // In-memory DB for ingestion status
 const ingestionStore = {};
@@ -15,6 +16,11 @@ const PRIORITY_MAP = {
   MEDIUM: 2,
   LOW: 3,
 };
+
+// test api for health check
+app.get("/", (req, res) => {
+    res.send("Ingestion API is running successfully");
+})
 
 // Ingestion APIS
 
